f0626ec97b169528209fa5c14d792cbdf5694cc4
level7webdev <6236517+level7webdev@users.noreply.github.com>
2020-10-29 11:03:06 +0000

    Add IEX credentials & lookup method is Stock model

---
+++ app/models/stock.rb
@@ -0,0 +1,10 @@
+class Stock < ApplicationRecord
+
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.IEX[:publishable_token],
+                                  secret_token: Rails.application.credentials.IEX[:secret_token],
+                                  endpoint: 'https://sandbox.iexapis.com/v1')
+    client.price(ticker_symbol)
+  end
+
+end
