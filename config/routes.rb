476eea912e03f8239d19b3f871e0de5a1397b07f
level7webdev <6236517+level7webdev@users.noreply.github.com>
2020-10-30 11:05:44 +0000

    Adds UserStock model for m2m association & styling

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   devise_for :users
   
   get 'my_portfolio', to: 'users#my_portfolio'
-
+  
   get 'search_stock', to: 'stocks#search'
-
+  
+  resources :user_stocks
+  
 end
