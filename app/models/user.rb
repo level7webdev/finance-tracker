33d929a3a1598733baaea5e85617ab1a712daca2
level7webdev <6236517+level7webdev@users.noreply.github.com>
2020-11-01 16:01:20 +0000

    Adds first and last name to user model and views

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,15 +10,20 @@ class User < ApplicationRecord
   def under_stock_limit?
     stocks.count < 10
   end
-
+  
   def stock_already_tracked?(ticker_symbol)
     stock = Stock.check_db(ticker_symbol)
     return false unless stock
     stocks.where(id: stock.id).exists?
   end
-
+  
   def can_track_stock?(ticker_symbol)
     under_stock_limit? && !stock_already_tracked?(ticker_symbol)
   end
-
+        
+  def full_name
+    return "#{first_name} #{last_name}" if first_name || last_name
+    "Anonymous"
+  end
+        
 end
